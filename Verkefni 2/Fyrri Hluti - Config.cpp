125b5c99da338f5a697ab8d1e12f364fdfa43fcc
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2022-03-22 09:10:21 +0000

    Update Fyrri Hluti - Config.cpp

--- Verkefni 2/Fyrri Hluti - Config.cpp
+++ Verkefni 2/Fyrri Hluti - Config.cpp
@@ -1,57 +1,114 @@
-/*----------------------------------------------------------------------------*/
-/*                                                                            */
-/*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\dagur                                            */
-/*    Created:      Tue Mar 22 2022                                           */
-/*    Description:  V5 project                                                */
-/*                                                                            */
-/*----------------------------------------------------------------------------*/
-
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    3, 4            
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-int userThread()
-{
-   while(1) // main loop
-   {
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
 
-    vexcodeInit();
-    Drivetrain.setDriveVelocity(50,rpm);
-    Drivetrain.setTurnVelocity(50,rpm);
-    Drivetrain.driveFor(forward, 500, mm);
-    
-    bool listi[14] ={1,0,0,1,1,0,1,1,0,1,1,1,0,0};
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT3, ratio18_1, false);
+motor RightDriveSmart = motor(PORT4, ratio18_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+controller Controller1 = controller(primary);
+motor ArmMotor = motor(PORT19, ratio18_1, false);
+motor ClawMotor = motor(PORT20, ratio18_1, false);
 
-    for(int i =0;i<14;i++){
-      Drivetrain.driveFor(forward, 500, mm);
-      if (listi[i] == 0){
-        Drivetrain.turnFor(left, 90, degrees);
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool Controller1UpDownButtonsControlMotorsStopped = true;
+bool Controller1XBButtonsControlMotorsStopped = true;
+bool DrivetrainLNeedsToBeStopped_Controller1 = true;
+bool DrivetrainRNeedsToBeStopped_Controller1 = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3 + Axis1
+      // right = Axis3 - Axis1
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position() + Controller1.Axis1.position();
+      int drivetrainRightSideSpeed = Controller1.Axis3.position() - Controller1.Axis1.position();
+      
+      // check if the value is inside of the deadband range
+      if (drivetrainLeftSideSpeed < 5 && drivetrainLeftSideSpeed > -5) {
+        // check if the left motor has already been stopped
+        if (DrivetrainLNeedsToBeStopped_Controller1) {
+          // stop the left drive motor
+          LeftDriveSmart.stop();
+          // tell the code that the left motor has been stopped
+          DrivetrainLNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the left motor nexttime the input is in the deadband range
+        DrivetrainLNeedsToBeStopped_Controller1 = true;
       }
-      else;
-        Drivetrain.turnFor(right, 90, degrees);
+      // check if the value is inside of the deadband range
+      if (drivetrainRightSideSpeed < 5 && drivetrainRightSideSpeed > -5) {
+        // check if the right motor has already been stopped
+        if (DrivetrainRNeedsToBeStopped_Controller1) {
+          // stop the right drive motor
+          RightDriveSmart.stop();
+          // tell the code that the right motor has been stopped
+          DrivetrainRNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the right motor next time the input is in the deadband range
+        DrivetrainRNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainLNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainRNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+      // check the ButtonUp/ButtonDown status to control ClawMotor
+      if (Controller1.ButtonUp.pressing()) {
+        ClawMotor.spin(forward);
+        Controller1UpDownButtonsControlMotorsStopped = false;
+      } else if (Controller1.ButtonDown.pressing()) {
+        ClawMotor.spin(reverse);
+        Controller1UpDownButtonsControlMotorsStopped = false;
+      } else if (!Controller1UpDownButtonsControlMotorsStopped) {
+        ClawMotor.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1UpDownButtonsControlMotorsStopped = true;
+      }
+      // check the ButtonX/ButtonB status to control ArmMotor
+      if (Controller1.ButtonX.pressing()) {
+        ArmMotor.spin(forward);
+        Controller1XBButtonsControlMotorsStopped = false;
+      } else if (Controller1.ButtonB.pressing()) {
+        ArmMotor.spin(reverse);
+        Controller1XBButtonsControlMotorsStopped = false;
+      } else if (!Controller1XBButtonsControlMotorsStopped) {
+        ArmMotor.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1XBButtonsControlMotorsStopped = true;
+      }
     }
-      Drivetrain.driveFor(forward, 500, mm);
-      this_thread::sleep_for( 10 );
-    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
 }
 
-int main()
-{
-    vex::thread ut(userThread);
-
-    while(1) // emergency stop loop
-    {
-      if( BumperA || Controller1.ButtonA.pressing())
-      {
-            ut.interrupt();  // stop user thread
-            return 0;
-      }
-      this_thread::sleep_for( 10 );
-    }
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+}
