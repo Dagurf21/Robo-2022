6db850878b2f9fa775f2c797476574d6ebb50aad
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2022-03-29 10:49:08 +0000

    Update Fyrri Hluti - Config.cpp

--- Verkefni 4/Fyrri Hluti - Config.cpp
+++ Verkefni 4/Fyrri Hluti - Config.cpp
@@ -1,1 +1,61 @@
+#include "vex.h"
 
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT3, ratio18_1, false);
+motor RightDriveSmart = motor(PORT4, ratio18_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+line LineTrackerC = line(Brain.ThreeWirePort.C);
+line LineTrackerD = line(Brain.ThreeWirePort.D);
+line LineTrackerE = line(Brain.ThreeWirePort.E);
+controller Controller1 = controller(primary);
+bumper BumperA = bumper(Brain.ThreeWirePort.A);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT3, ratio18_1, false);
+motor RightDriveSmart = motor(PORT4, ratio18_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+line LineTrackerC = line(Brain.ThreeWirePort.C);
+line LineTrackerD = line(Brain.ThreeWirePort.D);
+line LineTrackerE = line(Brain.ThreeWirePort.E);
+controller Controller1 = controller(primary);
+bumper BumperA = bumper(Brain.ThreeWirePort.A);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
