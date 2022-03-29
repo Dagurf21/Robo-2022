89cd32e565a80f464f7eb49927b961064858c490
Dagur Freyr Guðbrandsson <66073735+Dagurf21@users.noreply.github.com>
2022-03-29 10:50:32 +0000

    Update Fyrri Hluti - Config.cpp

--- Verkefni 4/Fyrri Hluti - Config.cpp
+++ Verkefni 4/Fyrri Hluti - Config.cpp
@@ -1,89 +1,61 @@
 #include "vex.h"
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Drivetrain           drivetrain    3, 4            
-// LineTrackerC         line          C               
-// LineTrackerD         line          D               
-// LineTrackerE         line          E               
-// Controller1          controller                    
-// BumperA              bumper        A               
-// ---- END VEXCODE CONFIGURED DEVICES ----
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT3, ratio18_1, false);
+motor RightDriveSmart = motor(PORT4, ratio18_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+line LineTrackerC = line(Brain.ThreeWirePort.C);
+line LineTrackerD = line(Brain.ThreeWirePort.D);
+line LineTrackerE = line(Brain.ThreeWirePort.E);
+controller Controller1 = controller(primary);
+bumper BumperA = bumper(Brain.ThreeWirePort.A);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}#include "vex.h"
 
 using namespace vex;
-float LTLeft = 0;
-float LTCenter = 0;
-float LTRight = 0;
-float threshold = 30;
+using signature = vision::signature;
+using code = vision::code;
 
-float LTLeftOld = 0;
-float LTCenterOld = 0;
-float LTRightOld = 0;
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
 
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT3, ratio18_1, false);
+motor RightDriveSmart = motor(PORT4, ratio18_1, true);
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+line LineTrackerC = line(Brain.ThreeWirePort.C);
+line LineTrackerD = line(Brain.ThreeWirePort.D);
+line LineTrackerE = line(Brain.ThreeWirePort.E);
+controller Controller1 = controller(primary);
+bumper BumperA = bumper(Brain.ThreeWirePort.A);
 
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
 
-
-int userThread(){
-  while(1){
-  vexcodeInit();
-    Drivetrain.setDriveVelocity(7, rpm);
-    Drivetrain.setTurnVelocity(7, rpm);
-    Drivetrain.driveFor(forward, 100, mm);
-    while(true){
-      LTLeft = LineTrackerC.reflectivity();
-      LTCenter = LineTrackerD.reflectivity();
-      LTRight = LineTrackerE.reflectivity();
-      Drivetrain.setDriveVelocity(7, rpm);
-      Drivetrain.setTurnVelocity(7, rpm);
-
-      if(LTLeft < threshold and LTCenter < threshold and LTRight < threshold){
-        Drivetrain.drive(forward);
-      }
-      if(LTLeft < threshold and LTCenter < threshold and LTRight > threshold){
-          Drivetrain.turn(left);
-          Drivetrain.drive(forward);
-      }
-      if(LTLeft > threshold and LTCenter < threshold and LTRight < threshold){
-          Drivetrain.turn(right);
-          Drivetrain.drive(forward);
-      }
-      if(LTLeft < threshold and LTCenter > threshold and LTRight > threshold){
-          Drivetrain.setTurnVelocity(20, rpm);
-          Drivetrain.setDriveVelocity(10, rpm);          
-          Drivetrain.turn(left);
-          Drivetrain.drive(forward);
-      }
-      if(LTLeft > threshold and LTCenter > threshold and LTRight < threshold){
-          Drivetrain.setTurnVelocity(20, rpm);
-          Drivetrain.setDriveVelocity(10, rpm);
-          Drivetrain.turn(right);
-          Drivetrain.drive(forward);
-      }
-      if(LTLeft > threshold and LTCenter > threshold and LTRight > threshold){
-        Drivetrain.stop();
-      }
-    }
-    this_thread::sleep_for( 10 );
-  }
-}
-
-int main()
-{
-    vex::thread ut(userThread);
-
-    while(1) // emergency stop loop
-    {
-      if( BumperA || Controller1.ButtonA.pressing())
-      {
-            ut.interrupt();  // stop user thread
-            Brain.Screen.print("hallo");
-            wait(50, msec);
-            Brain.Screen.setCursor(1,1);
-            Brain.Screen.clearScreen();
-            Drivetrain.stop();
-
-      }
-      this_thread::sleep_for( 10 );
-    }
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
 }
